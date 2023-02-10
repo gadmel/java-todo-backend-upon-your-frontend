c0b6f6ac40bf1bac6b8463e94722999bb4299ccd
gadmel <abramov.gleb@gmail.com>
2023-02-10 11:29:22 +0100

    Create a GetMappings acc. to given routes.

--- backend/src/main/java/com/example/backend/controller/TodoController.java
+++ backend/src/main/java/com/example/backend/controller/TodoController.java
@@ -2,10 +2,14 @@ package com.example.backend.controller;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 import com.example.backend.model.Todo;
 import com.example.backend.service.TodoService;
@@ -16,6 +20,16 @@ import com.example.backend.service.TodoService;
 public class TodoController {
 
 	private TodoService todoService;
+
+	@GetMapping("")
+	public List<Todo> getTodos() {
+		return todoService.getTodos();
+	}
+
+	@GetMapping("/{id}")
+	public Todo getTodoById(@PathVariable String id) {
+		return todoService.getTodoById(id);
+	}
 
 	@PostMapping("")
 	public Todo postTodo(@RequestBody Todo incomingTodo) {
