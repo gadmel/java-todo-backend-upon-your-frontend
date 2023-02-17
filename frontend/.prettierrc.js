f64792c4f8c114207d3df16a543e4e982a91e411
gadmel <abramov.gleb@gmail.com>
2023-02-17 11:59:21 +0100

    Bootstrap vite app (react-ts-eslint-prettier), update React to 18, clean up, audit and fix

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  semi: false,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 90,
+  tabWidth: 3,
+  useTabs: true,
+  endOfLine: 'auto',
+};
+
+// {
+//   "arrowParens": "always",
+//   "bracketSpacing": true,
+//   "embeddedLanguageFormatting": "auto",
+//   "htmlWhitespaceSensitivity": "css",
+//   "insertPragma": false,
+//   "jsxBracketSameLine": false,
+//   "jsxSingleQuote": false,
+//   "proseWrap": "preserve",
+//   "quoteProps": "as-needed",
+//   "requirePragma": false,
+//   "printWidth": 100
+// }
---
+++ frontend/README.md
@@ -0,0 +1,20 @@
+# Vite + React + Typescript + Eslint + Prettier
+
+A starter for React with Typescript with the fast Vite and all static code testing with Eslint and formatting with Prettier.
+
+
+## Installation
+
+Clone the repo and run `yarn install`
+
+or Run command
+
+```
+npx degit TheSwordBreaker/vite-reactts-eslint-prettier project-name
+```
+
+## Start
+
+After the successfull installation of the packages: `yarn dev`
+
+Made with ❤️ by theSwordBreaker(we Destory all types of sword ⚡)
